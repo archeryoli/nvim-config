cbbeb891bcfba27aab04ab4058856c70f4ef5aa4
archeryoli <oliver.brandstetter2506@gmail.com>
2024-01-20 20:35:29 +0100

    init

---
+++ lua/plugins/barbar.lua
@@ -0,0 +1,21 @@
+return {
+	"romgrk/barbar.nvim",
+	dependencies = {
+		"lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
+		"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
+	},
+	init = function()
+		vim.g.barbar_auto_setup = false
+		local map = vim.api.nvim_set_keymap
+		local opts = { noremap = true, silent = true }
+
+		map("n", "<C-w>", "<Cmd>BufferClose<CR>", opts)
+	end,
+	opts = {
+		-- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+		-- animation = true,
+		-- insert_at_start = true,
+		-- …etc.
+	},
+	version = "^1.0.0", -- optional: only update when a new 1.x version is released
+}
---
+++ lua/vim-options.lua
@@ -0,0 +1,32 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+vim.g.mapleader = " "
+
+---@diagnostic disable-next-line: lowercase-global
+function map(mode, lhs, rhs, opts)
+	local options = { noremap = true }
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
+	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+map('i', 'kj', '<ESC>', {})
+map('n', '<leader>ff', ':Telescope find_files<CR>', {})
+map('i', '"' , '""<left>', {})
+map('i', "'" , "''<left>", {})
+map('i', '(', '()<left>', {})
+map('i', '[', '[]<left>', {})
+map('i', '{', '{}<left>', {})
+map('i', '{<CR>', '{<CR>}<ESC>O', {})
+map('i', '{;<CR>', '{<CR>};<ESC>O', {})
+
+
+-- vim.cmd("noremap ö l")
+-- vim.cmd("noremap l k")
+-- vim.cmd("noremap k j")
+-- vim.cmd("noremap j h")
+-- vim.cmd("inoremap jh <ESC>")
