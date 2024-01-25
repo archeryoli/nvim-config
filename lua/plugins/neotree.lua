4a14f4fe2f80f564077c43b3112ac382ea001939
archeryoli <oliver.brandstetter2506@gmail.com>
2024-01-25 08:35:41 +0100

    updated options for neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,12 +1,23 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", 
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
-  end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			close_if_last_window = false,
+			enable_diagnostics = true,
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					hide_dotfiles = false,
+					hide_gitignored = false,
+				},
+			},
+		})
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
+	end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,22 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      auto_installed = true,
-      highlight = { enable = true },
-      indent = { enable = true },
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+	},
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			auto_installed = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+		require("nvim-ts-autotag").setup({
+      enable = true,
+      filetypes = { "html", "xml", "vue" },
+      enable_rename = true,
+      enable_close = true,
+      enable_close_on_slash = true
     })
-  end,
+	end,
 }
